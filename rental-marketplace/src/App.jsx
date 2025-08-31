7007807fa6d946517babda20604a54ba9d706a55
llagasq <llagasq@yahoo.com>
2025-09-01 02:44:19 +1000

    Landing Page
    creating the landing page

---
+++ rental-marketplace/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './Header.jsx'
+
+function App() {
+    return (
+      <div style={{ paddingTop: "100px" }}>
+       <Header></Header>
+      </div>
+    );
+  
+  
+}
+
+export default App
